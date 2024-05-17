cdae786a3c4a47c51ab39a1085b920e9784c5e89
Mr. Developer <mr.developer.again@gmail.com>
2024-05-17 19:23:28 +0330

    test

--- app.py
+++ app.py
@@ -4,10 +4,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    test_list = [(35.722344529568815,51.420510209408484), (35.722344529568815 ,51.0510209408484 )]
-    return render_template("test.html", test=test_list)
-
-
+    data_list = [ {"x": 35.722344529568815,"y": 51.420510209408484}, {"x":35.722344529568815 ,"y":51.0510209408484 }]
+    return render_template("test.html", data=data_list)
 
 if __name__ == "__main__":
     app.run(debug=True)
